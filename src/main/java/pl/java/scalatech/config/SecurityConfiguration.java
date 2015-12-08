35a4e1d64c68c2307ab876dbb44e98c327c67687
Slawomir Borowiec <przodownik@tlen.pl>
2015-12-08 09:28:57 +0100

    first commit

--- src/main/java/pl/java/scalatech/config/SecurityConfiguration.java
+++ src/main/java/pl/java/scalatech/config/SecurityConfiguration.java
@@ -1,24 +1,27 @@
 package pl.java.scalatech.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import pl.java.scalatech.security.ApiAuthenticationEntryPoint;
+import pl.java.scalatech.security.ApiAuthenticationFailureHandler;
+import pl.java.scalatech.security.ApiAuthenticationSuccessHandler;
 
 
 @Configuration
 @ComponentScan(basePackages="pl.java.scalatech.security")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Value("${logout.url}")
-    private String logoutUrl;
-
+    @Autowired
+    private ApiAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private ApiAuthenticationFailureHandler authenticationFailureHandler;
+    @Autowired
+    private ApiAuthenticationSuccessHandler authenticationSuccessHandler;
 
 
     @Override
@@ -26,7 +29,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // @formatter:off
         web.ignoring().
         antMatchers("/assets/**")
-        .antMatchers("/resources/**")
+        .antMatchers("/css/**")
+        .antMatchers("/js/**")
+        .antMatchers("/images/**")
         .antMatchers("/favicon.ico")
         .antMatchers("/webjars/**");
         // @formatter:on
@@ -34,40 +39,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //http.requiresChannel().anyRequest().requiresSecure();
+        //http.httpBasic().and().requiresChannel().anyRequest().requiresSecure();
 
-        // @formatter:off
-        http.csrf().disable().headers().disable()
-          .authorizeRequests().antMatchers("/login","/logout","secContext","principal","/health","/console").permitAll()
-          .antMatchers("/simple/**").hasAnyRole("USER")
-          .antMatchers("/actuator/**").hasRole("ADMIN")
-          .antMatchers("/metrics/**").hasRole("ADMIN")
-          .antMatchers("/info/**").hasRole("ADMIN")
-          .antMatchers("/health/**").hasRole("ADMIN")
-          .antMatchers("/trace/**").hasRole("ADMIN")
-          .antMatchers("/dump/**").hasRole("ADMIN")
-          .antMatchers("/shutdown/**").hasRole("ADMIN")
-          .antMatchers("/beans/**").hasRole("ADMIN")
-          .antMatchers("/env/**").hasRole("ADMIN")
-          .antMatchers("/autoconfig/**").hasRole("ADMIN")
-          .anyRequest().authenticated().and()
-          .formLogin().loginPage("/login").defaultSuccessUrl("/welcome").failureUrl("/login?errorFormLogin").permitAll()
-          .and()
-          .logout().permitAll();
-
-
-           http.logout().logoutSuccessUrl(logoutUrl)
-          .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-          .logoutSuccessUrl(logoutUrl);
-
+        http.authorizeRequests().antMatchers("/api/**").authenticated();
+        http.csrf().disable();
+        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
+        http.formLogin().successHandler(authenticationSuccessHandler);
+        http.formLogin().failureHandler(authenticationFailureHandler);
 
 
           // @formatter:on
     }
-    @Autowired
-    public void configureGlobal(UserDetailsService userDetailsService,AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.userDetailsService(userDetailsService);
+   @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+     // @formatter:off
+
+        auth.inMemoryAuthentication().withUser("przodownik").password("slawek").roles("USER").and()
+                                     .withUser("admin").password("slawek").roles( "ADMIN");
+     // @formatter:on
     }
-
 }
